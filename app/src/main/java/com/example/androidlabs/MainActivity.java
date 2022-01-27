8d86e5c8bc075fdfd07892ffbbb1d3f0add33f99
AliOs <tigilla@outlook.com>
2022-01-26 21:58:26 -0500

    I created branch lab-2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main);
+    b=findViewById(R.id.myButton);
+    b.setOnClickListener((View.OnClickListener) this);
+}
+
+    public void clickToast(View v) {
+        Toast.makeText(getApplicationContext(), "Label has been updated", Toast.LENGTH_SHORT).show();
     }
 }
